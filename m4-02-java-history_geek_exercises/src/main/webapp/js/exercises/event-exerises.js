d4a97811019caf16cf48fd469dfe805333201fdf
Ahmed <Ahmiduwanur@gmail.com>
2018-11-25 22:01:19 -0500

    part two done

--- m4-02-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-02-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -1,5 +1,28 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 
+
 $(document).ready(function () {
 
+	$('#SameShipping').change(function(){
+		
+		    if(this.checked == true) {
+		    	var textValue = $('#BillingAddress1').val()
+				$('#ShippingAddress1').val(textValue)
+				$('#ShippingAddress2').val($('#BillingAddress2').val())
+				$('#ShippingCity').val($('#BillingCity').val())
+				$('#ShippingState').val($('#BillingState').val())
+				$('#ShippingPostalCode').val($('#BillingPostalCode').val())
+		    }
+		});
+	var grandTotalPrice = $('#grandtotal span').text();
+	
+	 $("input[type='radio']").on('change', function(){
+		 var grandTotal = parseFloat(grandTotalPrice.substring(1));
+		 if (this.checked == true){
+		 var radioValue = $('input[name="ShippingType"]:checked').attr('data-cost');
+	     var newTotal = parseFloat(radioValue) + grandTotal;
+	     $('#grandtotal span').html('$' + newTotal);
+		 }
+		 
+});
 });
